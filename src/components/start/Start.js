b7c3e420c06a0d53f315a1071e84108435df02c9
lukaszplac <lukasz.plac@gmail.com>
2017-12-29 14:57:08 +0100

    start animation changed to more fancy

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -4,7 +4,7 @@ import me from '../../../res/img/me-rounded.png';
 import ContentLeft from './submodules/ContentLeft';
 import ContentRight from './submodules/ContentRight';
 import contentJson from './content.json';
- 
+
 class Start extends React.Component {
 
 	constructor(props) {
@@ -15,19 +15,19 @@ class Start extends React.Component {
 	render() {
 		let contentArrayLeft = Object.values(contentJson.contentLeft);
 		let contentArrayRight = Object.values(contentJson.contentRight);
-		let contentParsedToDomElementsLeft = contentArrayLeft.map((item, i) => 
+		let contentParsedToDomElementsLeft = contentArrayLeft.map((item, i) =>
 					(<ContentLeft key={i} number={i} {...item} anim="fadeInLeft"/>)
 			);
-		let contentParsedToDomElementsRight = contentArrayRight.map((item, i) => 
+		let contentParsedToDomElementsRight = contentArrayRight.map((item, i) =>
 					(<ContentRight key={i} number={i} {...item} anim="fadeInRight"/>)
 			);
 		return(
-			
+
 			<div className={styles.start}>
 				<div className={'animated fadeInLeft ' + styles.info}>
 					{contentParsedToDomElementsLeft}
 				</div>
-				<div className={'animated fadeInUp ' + styles.image}>
+				<div className={'animated flip ' + styles.image}>
 					<img src={me}></img>
 				</div>
 				<div className={'animated fadeInRight ' + styles.info}>
