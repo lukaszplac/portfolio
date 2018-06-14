7c9ffaf9a38e0620c8693f06bdf942331b8bbc12
lukaszplac123 <lukasz.plac@gmail.com>
2018-06-14 23:53:00 +0200

    facelifting in progress

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,12 +6,45 @@ import * as Fa from 'react-icons/lib/fa'
 class Navigation extends React.Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			naviToggle: true
+		};
 	}
+
+	forceRerender() {
+		this.setState({naviToggle: false});
+		setTimeout(() => {
+			this.setState({naviToggle: true});
+		}, 500);
+	}
+
 	render() {
+	let naviClass = this.state.naviToggle ? [styles.navi1, 'animated fadeInLeft'].join(' '): 
+		styles.naviReset;
+	console.log(naviClass);
 	return(
 		<div>
-			<div className={[styles.navi, 'animated fadeInLeft'].join(' ')}>
-				<div className={styles.logo}>
+			<div className={naviClass}>
+					<div class={styles.naviArrows}>
+						<div className={styles.arrowContainer}>
+							<Fa.FaAngleLeft className={styles.arrowLeft} size="25" />
+						</div>
+						<div className={styles.arrowContainer}>
+							<Fa.FaAngleLeft className={styles.arrowLeft} size="25" />
+						</div>
+						<div className={styles.arrowContainer}>
+							<Fa.FaAngleLeft className={styles.arrowLeft} size="25" />
+						</div>
+					</div>
+					<div className={styles.links}>
+						<ul>
+							<li><span onClick={() => this.forceRerender()}><Link to={'/about'}>about</Link></span></li>
+							<li><span onClick={() => this.forceRerender()}><Link to={'/portfolio'}>portfolio</Link></span></li>
+							<li><span onClick={() => this.forceRerender()}><Link to={'/contact'}>contact</Link></span></li>
+						</ul>
+					</div>
+
+				{/* <div className={styles.logo}>
 					<h3><Link className={styles.link} to={'/'}>{"<code/>"}</Link></h3>
 				</div>
 				<div className={styles.links}>
@@ -25,7 +58,7 @@ class Navigation extends React.Component {
 					<a href="http://www.facebook.com/lukaszplac123" target="_blank"><Fa.FaFacebookOfficial className={styles.icon} size='25'/></a>
 					<a href="http://www.github.com/lukaszplac" target="_blank"><Fa.FaGithub className={styles.icon} size='25' /></a>
 					<a href="http://www.linkedin.com/in/lukasz-plac" target="_blank"><Fa.FaLinkedin className={styles.icon} size='25' /></a>
-				</div>
+				</div> */}
 			</div>
 		</div>
 		)
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import styles from './index.scss';
 
 ReactDOM.render(
 	<Router>
